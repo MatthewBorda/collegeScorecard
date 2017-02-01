9ca614d4aefc43b6fd3b617e87e623d45a699197
matthewmborda <matthewmborda@gmail.com>
2017-02-01 18:18:23 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name collegeScorecardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the collegeScorecardApp
+ */
+angular.module('collegeScorecardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
