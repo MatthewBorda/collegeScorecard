19e10f331bd5409fc7a3129af6a335ad7b44ea82
matthewmborda <matthewmborda@gmail.com>
2017-02-05 16:32:56 -0500

    API Test connection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,13 +7,12 @@
  * # MainCtrl
  * Controller of the collegeScorecardApp
  */
-angular.module('collegeScorecardApp',['rzModule'])
-  .controller('MainCtrl', function ($scope, search, priceSlider) {
+angular.module('collegeScorecardApp')
+  .controller('MainCtrl', function ($scope, search) {
 //     this.awesomeThings = [
 //       'HTML5 Boilerplate',
 //       'AngularJS',
 //       'Karma'
 //     ];
   $scope.search = search.query();
-  $scope.priceSlider = 150;
   });
