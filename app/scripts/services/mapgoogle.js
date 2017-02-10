6afea8736795e122f227b2a15d4543c1fa879c32
matthewmborda <matthewmborda@gmail.com>
2017-02-10 15:22:49 -0500

    Google maps

--- app/scripts/services/mapgoogle.js
+++ app/scripts/services/mapgoogle.js
@@ -7,10 +7,38 @@
  * # mapGoogle
  * Factory in the collegeScorecardApp.
  */
-angular.module('collegeScorecardApp',['uiGmapgoogle-maps'])
-  .factory('mapGoogle', function ($scope) {
-    // Service logic
-    // ...
+angular.module('collegeScorecardApp',['google-maps'])
+	function ExampleController ($scope) {
+		angular.extend($scope, {
+			centerProperty: {
+				lat: 45,
+				lng: -73
+			},
+			zoomProperty: 8,
+			markersProperty: [ {
+					latitude: 45,
+					longitude: -74
+				}],
+			clickedLatitudeProperty: null,	
+			clickedLongitudeProperty: null,
+		});
+  }
 
-$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-  });
+//   .factory('mapGoogle', function ($scope) {
+//     // Service logic
+//   	angular.extend($scope, {
+// 			centerProperty: {
+// 				lat: 45,
+// 				lng: -73
+// 			},
+// 			zoomProperty: 8,
+// 			markersProperty: [ {
+// 					latitude: 45,
+// 					longitude: -74
+// 				}],
+// 			clickedLatitudeProperty: null,	
+// 			clickedLongitudeProperty: null,
+// 		});
+// }
+
+// $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
