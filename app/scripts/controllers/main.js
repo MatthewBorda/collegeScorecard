9730a08c3a1a9d52b1e04901ba65117b2c426817
matthewmborda <matthewmborda@gmail.com>
2017-02-10 16:20:59 -0500

    Google maps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,18 +7,18 @@
  * # MainCtrl
  * Controller of the collegeScorecardApp
  */
-angular.module('collegeScorecardApp',['ngMaps'])
-  .controller('MainCtrl', function ($scope, map) {
+angular.module('collegeScorecardApp')
+  .controller('MainCtrl', function ($scope, search) {
 //     this.awesomeThings = [
 //       'HTML5 Boilerplate',
 //       'AngularJS',
 //       'Karma'
 //     ];
 //   search, marker
-//   $scope.search = search.query();
-  $scope.map = {
-    center: [40.7, -74]
-  }
+  $scope.search = search.query();
+//   $scope.map = {
+//     center: [40.7, -74]
+//   }
 //   $scope.marker = {
 //     position: [40.7, -74]
 //   }
