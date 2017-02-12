eb8e19a089b493f8d2373a43c23954d18adb7cba
matthewmborda <matthewmborda@gmail.com>
2017-02-11 23:22:38 -0500

    Search box

--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -13,7 +13,7 @@ angular.module('collegeScorecardApp')
       // ...
 
       // Public API here
-      return $resource('https://api.data.gov/ed/collegescorecard/v1/schools?school.name=:query&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ', {}, {
+      return $resource('https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=:query&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ&_fields=id,school.name', {}, {
         find: {
           method: 'GET',
           params: {
