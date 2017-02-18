aeffcb42fe32587f1b914b4a00f5e32b424d3e93
matthewmborda <matthewmborda@gmail.com>
2017-02-18 18:30:51 -0500

    Slider setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,100 +8,106 @@
  * Controller of the collegeScorecardApp
  , ['ui.grid']*/
 angular.module('collegeScorecardApp')
-  .controller('MainCtrl', function ($scope, search) {
-$scope.schoolsFound = search.find()
+  .controller('MainCtrl', function($scope, search) {
+    $scope.schoolsFound = search.find()
 
-$scope.findSchools = function() {
-  $scope.schoolFound = search.find({
-  query: $scope.school
-    });
-  $scope.searchQuery =  $scope.school; 
-  }
-
- $scope.degree = ['Any', "Two-Year (Associates)", "Four-Year (Bachelor's)"];
-    $scope.degreeselected = function (item) {
-      $scope.selectedItem = item;
-        alert($scope.selectedItem);
+    $scope.findSchools = function() {
+      $scope.schoolFound = search.find({
+        query: $scope.school
+      });
+      $scope.searchQuery = $scope.school;
     }
- $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+    $scope.degree = ['Any', "Two-Year (Associates)", "Four-Year (Bachelor's)"];
+    $scope.degreeselected = function(item) {
+      $scope.selectedItem = item;
+      alert($scope.selectedItem);
+    }
+    $scope.map = {
+      center: {
+        latitude: 45,
+        longitude: -73
+      },
+      zoom: 8
+    };
 
 
-  $scope.acceptanceSlider = {
-    minValue: 0,
-    maxValue: 100,
-    options: {
+    $scope.acceptanceSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
         floor: 0,
         ceil: 100,
         step: 1,
         minRange: 10,
         maxRange: 100,
         pushRange: true
-    }
-};
-  $scope.SATslider = {
-    minValue: 0,
-    maxValue: 1600,
-    options: {
+      }
+    };
+    $scope.SATslider = {
+      minValue: 0,
+      maxValue: 1600,
+      options: {
         floor: 0,
         ceil: 1600,
         step: 1,
         minRange: 100,
         maxRange: 1600,
         pushRange: true
-    }
-};
-  $scope.ACTslider = {
-    minValue: 0,
-    maxValue: 36,
-    options: {
+      }
+    };
+    $scope.ACTslider = {
+      minValue: 0,
+      maxValue: 36,
+      options: {
         floor: 0,
         ceil: 36,
         step: 1,
         minRange: 2,
         maxRange: 36,
         pushRange: true
-    }
-};
-  $scope.raceSlider = {
-    minValue: 0,
-    maxValue: 100,
-    options: {
+      }
+    };
+    $scope.raceSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
         floor: 0,
         ceil: 100,
         step: 1,
         minRange: 10,
         maxRange: 100,
         pushRange: true
-    }
-};
-  $scope.genSlider = {
-    minValue: 0,
-    maxValue: 100,
-    options: {
+      }
+    };
+    $scope.genSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
         floor: 0,
         ceil: 100,
         step: 1,
         minRange: 10,
         maxRange: 100,
         pushRange: true
-    }
-};
-  $scope.pellSlider = {
-    minValue: 0,
-    maxValue: 100,
-    options: {
+      }
+    };
+    $scope.pellSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
         floor: 0,
         ceil: 100,
         step: 1,
         minRange: 10,
         maxRange: 100,
         pushRange: true
-    }
-};
-  $scope.costSlider  = {
-    minValue: 0,
-    maxValue: 100000,
-    options: {
+      }
+    };
+    $scope.costSlider = {
+      minValue: 0,
+      maxValue: 100000,
+      options: {
         showTicksValues: true,
         floor: 0,
         ceil: 100000,
@@ -109,13 +115,62 @@ $scope.findSchools = function() {
         minRange: 1000,
         maxRange: 100,
         pushRange: true
-    }
-};
-  
-  $scope.tuitionSlider = 100;
-  $scope.priceSlider = 100;
-  $scope.debtSlider = 100;
-  $scope.loanSlider = {
+      }
+    };
+
+    $scope.tuitionSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
+        floor: 0,
+        ceil: 100,
+        step: 1,
+        minRange: 10,
+        maxRange: 100,
+        pushRange: true
+      }
+    };
+
+    $scope.priceSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
+        floor: 0,
+        ceil: 100,
+        step: 1,
+        minRange: 10,
+        maxRange: 100,
+        pushRange: true
+      }
+    };
+
+    $scope.debtSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
+        floor: 0,
+        ceil: 100,
+        step: 1,
+        minRange: 10,
+        maxRange: 100,
+        pushRange: true
+      }
+    };
+
+    $scope.loanSlider = {
+      minValue: 0,
+      maxValue: 100,
+      options: {
+        floor: 0,
+        ceil: 100,
+        step: 1,
+        minRange: 10,
+        maxRange: 100,
+        pushRange: true
+      }
+    };
+
+    $scope.earning7Slider = {
     minValue: 0,
     maxValue: 100,
     options: {
@@ -128,9 +183,31 @@ $scope.findSchools = function() {
     }
 };
 
-  $scope.earning7Slider = 100;
-  $scope.earning3Slider = 100;
-  $scope.earning1Slider = 100;
+    $scope.earning3Slider = {
+    minValue: 0,
+    maxValue: 100,
+    options: {
+        floor: 0,
+        ceil: 100,
+        step: 1,
+        minRange: 10,
+        maxRange: 100,
+        pushRange: true
+    }
+};
+
+    $scope.earning1Slider = {
+    minValue: 0,
+    maxValue: 100,
+    options: {
+        floor: 0,
+        ceil: 100,
+        step: 1,
+        minRange: 10,
+        maxRange: 100,
+        pushRange: true
+    }
+};
+
 
   });
-
