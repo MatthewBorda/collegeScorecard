b325dc5bc5b08c571c2c19219a3239fc5fe072ba
matthewmborda <matthewmborda@gmail.com>
2017-02-25 21:16:46 -0500

    resource updates

--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -10,17 +10,17 @@
 angular.module('collegeScorecardApp')
   .factory('search', function($resource) {
       // Service logic
-      // ...
+      // 'https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=:query&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ'
 
       // Public API here
-      return $resource('https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=:query&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ', {}, {
-        find: {
-          method: 'GET',
-          params: {
-            query: 'Seattle',
+      return $resource('https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ', {}, {
+//         find: {
+//           method: 'GET',
+//           params: {
+//             query: 'Seattle',
            
-          },
-          isArray: false
-        }
+//           },
+//           isArray: false
+//         }
       });
   });
