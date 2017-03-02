874dda33231a91d5c1cfa945c08173b1bd16c78c
matthewmborda <matthewmborda@gmail.com>
2017-03-01 19:40:35 -0500

    grid updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,32 +23,32 @@ angular.module('collegeScorecardApp')
     //   Local storage
 
   $scope.saveSchool = function(schools) {
-          $localStorage.compareSchools = {
-            'id': schools.id
-          };
+//           $localStorage.compareSchools = {
+//             'id': schools.id
+//           };
        
 //         ,        'name':schoolFound.results.school.name
 // schoolResults.results.school.id
 // $scope.schoolFound.results.school.id,
    
-//       if (!$localStorage.compareSchools) {
-// //         $localStorage.compareSchools[schools.id]=schools;
-//                  $localStorage.compareSchools=schools;
-//       } else {
+      if (!$localStorage.compareSchools) {
+//         $localStorage.compareSchools[schools.id]=schools;
+                 $localStorage.compareSchools=[schools];
+      } else {
 
-//         var save = true; // Initialize the save decision variable.
-//         for (var $i = 0; $i < $localStorage.compareSchools.length; $i++) {
-//           if ($localStorage.compareSchools[$i].id == schools.id) {
-//             save = false;
-//           }
-//         }
-//         if (save === true) {
-//             $localStorage.compareSchools.push(schools);
-// //           $localStorage.compareSchools[schools.id]=schools;
-//         } else {
-//           console.log('School already saved');
-//         }
-//       }
+        var save = true; // Initialize the save decision variable.
+        for (var $i = 0; $i < $localStorage.compareSchools.length; $i++) {
+          if ($localStorage.compareSchools[$i].id == schools.id) {
+            save = false;
+          }
+        }
+        if (save === true) {
+            $localStorage.compareSchools.push([schools]);
+//           $localStorage.compareSchools[schools.id]=schools;
+        } else {
+          console.log('School already saved');
+        }
+      }
     };
 //   Local storage
     $scope.savedSchools = $localStorage.compareSchools;
