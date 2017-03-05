57ebbd34780650472442e01242f1068496f2a78c
matthewmborda <matthewmborda@gmail.com>
2017-03-05 10:24:40 -0500

    Grid formatting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -37,28 +37,28 @@ angular.module('collegeScorecardApp')
         //Student
         'Undergraduates': schools['2014'].student.size,
         'Female %': schools['2014'].student.demographics.women *100, 
-        'White %': schools['2014'].student.demographics.race_ethnicity.white,
-        'Pell %': schools['2014'].student.students_with_pell_grant,
-        'First Gen %': schools['2014'].student.share_firstgeneration,
-        'Retenion %': schools['2014'].student.retention_rate.overall.full_time,
+        'White %': schools['2014'].student.demographics.race_ethnicity.white*100,
+        'Pell %': schools['2014'].student.students_with_pell_grant*100,
+        'First Gen %': schools['2014'].student.share_firstgeneration*100,
+        'Retenion %': schools['2014'].student.retention_rate.overall.full_time*100,
         //Admissions
-        'Admit %': schools['2014'].admissions.admission_rate.overall,
-        'SAT AVG': schools['2014'].admissions.sat_scores.overall,
+        'Admit %': schools['2014'].admissions.admission_rate.overall*100,
+        'SAT AVG': schools['2014'].admissions.sat_scores.average.overall,
         'ACT MID': schools['2014'].admissions.act_scores.midpoint.cumulative,
         //Repayment
         
         //Aid
         'Median Debt': schools['2014'].aid.median_debt.completers.overall,
-        'With Loan %': schools['2014'].aid.students_with_any_loan,
+        'With Loan %': schools['2014'].aid.students_with_any_loan*100,
         //Cost
         'Net Price': schools['2014'].cost.avg_net_price.overall,
         'Cost Attendance': schools['2014'].cost.attendance.academic_year,
         'Tuition': schools['2014'].cost.tuition.in_state,
         //Completion
           //4 year
-          'Graduated in 4 yrs': schools['2007'].completion.completion_rate_4yr_100nt,
+          'Graduated in 4 yrs': schools['2007'].completion.completion_rate_4yr_100nt*100,
           //6 year
-          'Graduated in 6 yrs': schools['2007'].completion.completion_rate_4yr_150nt
+          'Graduated in 6 yrs': schools['2007'].completion.completion_rate_4yr_150nt*100
         
       };
       if (!$localStorage.compareSchools) {
@@ -83,26 +83,27 @@ angular.module('collegeScorecardApp')
                         enableColumnResizing: true,
                         columnDefs: [
                          {field: 'id', width:0 },
+                           { name: 'Remove', cellTemplate:'<button class="btn primary" ng-click="grid.appScope.showMe()">Remove</button>' },
                         {field: 'Name', width:200 },
                         //School
                         {field: 'State', width:100 },
-                        {field: 'Undergraduates', width:100, cellFilter: 'number: 0'  },
+                        {field: 'Undergraduates', width:125, cellFilter: 'number: 0'  },
                           //Ideal: minority school fields collapsed
                         //Earnings
                         {field: 'Median Earnings at 10yrs', width:100, cellFilter: 'currency:"$":0' },
-                        {field: 'Above HS equiv at 10yrs', width:100 },
+                        {field: 'Above HS equiv at 10yrs', displayName: 'Above HS equiv at 10yrs', width:100 },
                         //Academics
                           //Ideal: Top 3 largest programs             
                         //Student
                         {field: 'Female %', width:100, cellFilter: 'number: 0'  },
-                        {field: 'White %', width:100 },
-                        {field: 'Pell %', width:100 },
-                        {field: 'First Gen %', width:100 },
-                        {field: 'Retenion %', width:100 },
+                        {field: 'White %', width:100, cellFilter: 'number: 0' },
+                        {field: 'Pell %', width:100, cellFilter: 'number: 0' },
+                        {field: 'First Gen %', width:100, cellFilter: 'number: 0' },
+                        {field: 'Retenion %', width:100, cellFilter: 'number: 0' },
                         //Admissions
-                        {field: 'Admit %', width:100 },
-                        {field: 'SAT AVG', width:100 },
-                        {field: 'ACT MID', width:100 },
+                        {field: 'Admit %', width:100, cellFilter: 'number: 0' },
+                        {field: 'SAT AVG', width:100, cellFilter: 'number: 0' },
+                        {field: 'ACT MID', width:100, cellFilter: 'number: 0' },
                         //Repayment
                         //Aid
                         {field: 'Median Debt', width:100, cellFilter: 'currency:"$":0'  },
@@ -113,11 +114,14 @@ angular.module('collegeScorecardApp')
                         {field: 'Tuition', width:100, cellFilter: 'currency:"$":0'  },
                         //Completion
                           //4 year
-                          {field: 'Graduated in 4 yrs', width:100 },
+                          {field: 'Graduated in 4 yrs', width:100, cellFilter: 'number: 0' },
                           //6 year100
-                          {field: 'Graduated in 6 yrs', width:100 }
+                          {field: 'Graduated in 6 yrs', width:100, cellFilter: 'number: 0' }
                                    ]};
-  
+   $scope.removeSchool = function(){
+                   alert($localStorage.compareSchools.id);
+                };
+
     $scope.savedSchools = $localStorage.compareSchools;
 //     $scope.gridOptions.columnDefs = [];
 //    $scope.gridOptions.data = $localStorage.compareSchools;
