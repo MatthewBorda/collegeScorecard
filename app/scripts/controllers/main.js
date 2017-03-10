0dda1b14ab0b4f38bf80c9d6ff59b7eabf00d92d
matthewmborda <matthewmborda@gmail.com>
2017-03-09 23:29:18 -0500

    search api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,62 +15,94 @@ angular.module('collegeScorecardApp')
 
     $scope.findSchools = function() {
       $scope.schoolFound = search.find({
-//         query: $scope.school,
+        //         query: $scope.school,
         minClass: $scope.classSlider.minValue,
-        maxClass: $scope.classSlider.maxValue 
+        maxClass: $scope.classSlider.maxValue,
+        minWomen: $scope.femaleSlider.minValue,
+        maxWomen: $scope.femaleSlider.maxValue,
+        minWhite: $scope.raceSlider.minValue,
+        maxWhite: $scope.raceSlider.maxValue,
+        minPell: $scope.pellSlider.minValue,
+        maxPell: $scope.pellSlider.maxValue,
+        minFirst: $scope.genSlider.minValue,
+        maxFirst: $scope.genSlider.maxValue,
+        minFull: $scope.retainedSlider.minValue,
+        maxFull: $scope.retainedSlider.maxValue,
+        minAdmit: $scope.acceptanceSlider.minValue,
+        maxAdmit: $scope.acceptanceSlider.maxValue,
+        minSAT: $scope.SATslider.minValue,
+        maxSAT: $scope.SATslider.maxValue,
+        //         
+        minDebt: $scope.debtSlider.minValue,
+        maxDebt: $scope.debtSlider.maxValue,
+        minLoan: $scope.borrowedSlider.minValue,
+        maxLoan: $scope.borrowedSlider.maxValue,
+        minPrice: $scope.netSlider.minValue,
+        maxPrice: $scope.netSlider.maxValue,
+        minTuition: $scope.tuitionSlider.minValue,
+        maxTuition: $scope.tuitionSlider.maxValue,
+        minCost: $scope.costSlider.minValue,
+        maxCost: $scope.costSlider.maxValue,
+        //         
+        minComp: $scope.completionSlider,
+        maxComp: $scope.completionSlider,
+        minEarn: $scope.earning7Slider,
+        maxEarn: $scope.earning7Slider,
+        minHS: $scope.hsSlider,
+        maxHS: $scope.hsSlider,
       });
-//      $scope.searchQuery = $scope.school;
-      $scope.searchmaxClass = $scope.classSlider.maxValue;
-      $scope.searchminClass = $scope.classSlider.minValue;
-      
-      
+      //      $scope.searchQuery = $scope.school;
+      //       $scope.searchmaxClass = $scope.classSlider.maxValue;
+      //       $scope.searchminClass = $scope.classSlider.minValue;
+
+
     }
 
     //   Local storage
     $scope.saveSchool = function(schools) {
       var schoolData = {
-         //Ideal: Year updated automatically
+        //Ideal: Year updated automatically
         'id': schools.id,
         'Name': schools.school.name,
         //School
         'State': schools.school.state,
-          //Ideal: minority school fields collapsed
+        //Ideal: minority school fields collapsed
         //Earnings
         'Median Earnings at 10yrs': schools['2012'].earnings['10_yrs_after_entry'].median,
         'Above HS equiv at 10yrs': schools['2012'].earnings['10_yrs_after_entry'].percent_greater_than_25000,
         //Academics
-          //Ideal: Top 3 largest programs             
+        //Ideal: Top 3 largest programs             
         //Student
         'Undergraduates': schools['2014'].student.size,
-        'Female %': schools['2014'].student.demographics.women *100, 
-        'White %': schools['2014'].student.demographics.race_ethnicity.white*100,
-        'Pell %': schools['2014'].student.students_with_pell_grant*100,
-        'First Gen %': schools['2014'].student.share_firstgeneration*100,
-        'Retenion %': schools['2014'].student.retention_rate.overall.full_time*100,
+        'Female %': schools['2014'].student.demographics.women * 100,
+        'White %': schools['2014'].student.demographics.race_ethnicity.white * 100,
+        'Pell %': schools['2014'].student.students_with_pell_grant * 100,
+        'First Gen %': schools['2014'].student.share_firstgeneration * 100,
+        'Retenion %': schools['2014'].student.retention_rate.overall.full_time * 100,
         //Admissions
-        'Admit %': schools['2014'].admissions.admission_rate.overall*100,
+        'Admit %': schools['2014'].admissions.admission_rate.overall * 100,
         'SAT AVG': schools['2014'].admissions.sat_scores.average.overall,
         'ACT MID': schools['2014'].admissions.act_scores.midpoint.cumulative,
         //Repayment
-        
+
         //Aid
         'Median Debt': schools['2014'].aid.median_debt.completers.overall,
-        'With Loan %': schools['2014'].aid.students_with_any_loan*100,
+        'With Loan %': schools['2014'].aid.students_with_any_loan * 100,
         //Cost
         'Net Price': schools['2014'].cost.avg_net_price.overall,
         'Cost Attendance': schools['2014'].cost.attendance.academic_year,
         'Tuition': schools['2014'].cost.tuition.in_state,
         //Completion
-          //4 year
-          'Graduated in 4 yrs': schools['2007'].completion.completion_rate_4yr_100nt*100,
-          //6 year
-          'Graduated in 6 yrs': schools['2007'].completion.completion_rate_4yr_150nt*100
-        
+        //4 year
+        'Graduated in 4 yrs': schools['2007'].completion.completion_rate_4yr_100nt * 100,
+        //6 year
+        'Graduated in 6 yrs': schools['2007'].completion.completion_rate_4yr_150nt * 100
+
       };
       if (!$localStorage.compareSchools) {
         $localStorage.compareSchools = [schoolData];
       } else {
-        var save = true; 
+        var save = true;
         for (var i = 0; i < $localStorage.compareSchools.length; i++) {
           if ($localStorage.compareSchools[i].id == schoolData.id) {
             save = false;
@@ -84,105 +116,142 @@ angular.module('collegeScorecardApp')
       }
     };
     //   Local storage
-  
-  $scope.gridOptions = { data:  $localStorage.compareSchools, 
-                        enableColumnResizing: true,
-                        columnDefs: [
-                         {field: 'id', width:0 },
-                           { name: 'Remove', cellTemplate:'<button class="btn primary" ng-click="grid.appScope.removeSchool()">Remove</button>', width:75 },
-                        {field: 'Name', width:200 },
-                        //School
-                        {field: 'State', width:100 },
-                        {field: 'Undergraduates', width:125, cellFilter: 'number: 0'  },
-                          //Ideal: minority school fields collapsed
-                        //Earnings
-                        {field: 'Median Earnings at 10yrs', width:100, cellFilter: 'currency:"$":0' },
-                        {field: 'Above HS equiv at 10yrs', displayName: 'Above HS equiv at 10yrs', width:100 },
-                        //Academics
-                          //Ideal: Top 3 largest programs             
-                        //Student
-                        {field: 'Female %', width:100, cellFilter: 'number: 0'  },
-                        {field: 'White %', width:100, cellFilter: 'number: 0' },
-                        {field: 'Pell %', width:100, cellFilter: 'number: 0' },
-                        {field: 'First Gen %', width:100, cellFilter: 'number: 0' },
-                        {field: 'Retenion %', width:100, cellFilter: 'number: 0' },
-                        //Admissions
-                        {field: 'Admit %', width:100, cellFilter: 'number: 0' },
-                        {field: 'SAT AVG', width:100, displayName: 'Average SAT Score', cellFilter: 'number: 0' },
-                        {field: 'ACT MID', width:100,displayName: 'ACT midpoint', cellFilter: 'number: 0' },
-                        //Repayment
-                        //Aid
-                        {field: 'Median Debt', width:100, cellFilter: 'currency:"$":0'  },
-                        {field: 'With Loan %', width:100, cellFilter: 'number: 0'  },
-                        //Cost
-                        {field: 'Net Price', width:100, cellFilter: 'currency:"$":0'  },
-                        {field: 'Cost Attendance', width:100, cellFilter: 'currency:"$":0'  },
-                        {field: 'Tuition', width:100, cellFilter: 'currency:"$":0'  },
-                        //Completion
-                          //4 year
-                          {field: 'Graduated in 4 yrs', width:100, cellFilter: 'number: 0' },
-                          //6 year100
-                          {field: 'Graduated in 6 yrs', width:100, cellFilter: 'number: 0' }
-                                   ]};
-   $scope.removeSchool = function(){
-                   alert("Remove from storage");
-                };
 
-    $scope.savedSchools = $localStorage.compareSchools;
-//     $scope.gridOptions.columnDefs = [];
-//    $scope.gridOptions.data = $localStorage.compareSchools;
-//        $scope.gridOptions = {
-    //       columnDefs: [
-    //         //       { name: 'School', cellTemplate: '<div class="ui-grid-cell-contents">{{ COL_FIELD.results.schools.name}} </div>' },
-    //         //         { name: 'School', cellTemplate: '<div class="ui-grid-cell-contents">{{ COL_FIELD.schoolFound.results.school.name}} </div>' },
-    //         {name: 'name'}, 
-    //         {name: 'id'}
-
-    //       ],
-//         data: $localStorage.compareSchools
-//        };
-  
-//   Data filters
-  
-    //Search filters 
-    $scope.degree = ['Any', "Two-Year (Associates)", "Four-Year (Bachelor's)"];
-    $scope.degreeselected = function(item) {
-      $scope.selectedItem = item;
-      alert($scope.selectedItem);
-    }
-    $scope.map = {
-      center: {
-        latitude: 45,
-        longitude: -73
-      },
-      zoom: 8
+    $scope.gridOptions = {
+      data: $localStorage.compareSchools,
+      enableColumnResizing: true,
+      columnDefs: [{
+          field: 'id',
+          width: 0
+        }, {
+          name: 'Remove',
+          cellTemplate: '<button class="btn primary" ng-click="grid.appScope.removeSchool()">Remove</button>',
+          width: 75
+        }, {
+          field: 'Name',
+          width: 200
+        },
+        //School
+        {
+          field: 'State',
+          width: 100
+        }, {
+          field: 'Undergraduates',
+          width: 125,
+          cellFilter: 'number: 0'
+        },
+        //Ideal: minority school fields collapsed
+        //Earnings
+        {
+          field: 'Median Earnings at 10yrs',
+          width: 100,
+          cellFilter: 'currency:"$":0'
+        }, {
+          field: 'Above HS equiv at 10yrs',
+          displayName: 'Above HS equiv at 10yrs',
+          width: 100
+        },
+        //Academics
+        //Ideal: Top 3 largest programs             
+        //Student
+        {
+          field: 'Female %',
+          width: 100,
+          cellFilter: 'number: 0'
+        }, {
+          field: 'White %',
+          width: 100,
+          cellFilter: 'number: 0'
+        }, {
+          field: 'Pell %',
+          width: 100,
+          cellFilter: 'number: 0'
+        }, {
+          field: 'First Gen %',
+          width: 100,
+          cellFilter: 'number: 0'
+        }, {
+          field: 'Retenion %',
+          width: 100,
+          cellFilter: 'number: 0'
+        },
+        //Admissions
+        {
+          field: 'Admit %',
+          width: 100,
+          cellFilter: 'number: 0'
+        }, {
+          field: 'SAT AVG',
+          width: 100,
+          displayName: 'Average SAT Score',
+          cellFilter: 'number: 0'
+        }, {
+          field: 'ACT MID',
+          width: 100,
+          displayName: 'ACT midpoint',
+          cellFilter: 'number: 0'
+        },
+        //Repayment
+        //Aid
+        {
+          field: 'Median Debt',
+          width: 100,
+          cellFilter: 'currency:"$":0'
+        }, {
+          field: 'With Loan %',
+          width: 100,
+          cellFilter: 'number: 0'
+        },
+        //Cost
+        {
+          field: 'Net Price',
+          width: 100,
+          cellFilter: 'currency:"$":0'
+        }, {
+          field: 'Cost Attendance',
+          width: 100,
+          cellFilter: 'currency:"$":0'
+        }, {
+          field: 'Tuition',
+          width: 100,
+          cellFilter: 'currency:"$":0'
+        },
+        //Completion
+        //4 year
+        {
+          field: 'Graduated in 4 yrs',
+          width: 100,
+          cellFilter: 'number: 0'
+        },
+        //6 year100
+        {
+          field: 'Graduated in 6 yrs',
+          width: 100,
+          cellFilter: 'number: 0'
+        }
+      ]
+    };
+    $scope.removeSchool = function() {
+      alert("Remove from storage");
     };
 
-    //grid controls
-
-    //   $scope.myData = [
-    //     {
-    //         "firstName": "Cox",
-    //         "lastName": "Carney",
-    //         "company": "Enormo",
-    //         "employed": true
-    //     },
-    //     {
-    //         "firstName": "Lorraine",
-    //         "lastName": "Wise",
-    //         "company": "Comveyer",
-    //         "employed": false
-    //     },
-    //     {
-    //         "firstName": "Nancy",
-    //         "lastName": "Waters",
-    //         "company": "Fuelton",
-    //         "employed": false
+    $scope.savedSchools = $localStorage.compareSchools;
+    //Search filters 
+    //     $scope.degree = ['Any', "Two-Year (Associates)", "Four-Year (Bachelor's)"];
+    //     $scope.degreeselected = function(item) {
+    //       $scope.selectedItem = item;
+    //       alert($scope.selectedItem);
     //     }
-    // ];
-    //
+    //     $scope.map = {
+    //       center: {
+    //         latitude: 45,
+    //         longitude: -73
+    //       },
+    //       zoom: 8
+    //     };
+
     // Slider controls
-  $scope.classSlider = {
+    $scope.classSlider = {
       minValue: 0,
       maxValue: 60000,
       options: {
@@ -194,67 +263,68 @@ angular.module('collegeScorecardApp')
         pushRange: true
       }
     };
-  $scope.borrowedSlider = {
+    $scope.borrowedSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
-  $scope.hsSlider = {
+    $scope.hsSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
-  $scope.retainedSlider = {
+
+    $scope.retainedSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
-  $scope.femaleSlider = {
+    $scope.femaleSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
-  $scope.acceptanceSlider = {
+    $scope.acceptanceSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
-  $scope.SATslider = {
+    $scope.SATslider = {
       minValue: 0,
       maxValue: 1600,
       options: {
@@ -266,39 +336,39 @@ angular.module('collegeScorecardApp')
         pushRange: true
       }
     };
-  $scope.raceSlider = {
+    $scope.raceSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
     $scope.genSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
     $scope.pellSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
@@ -315,7 +385,7 @@ angular.module('collegeScorecardApp')
         pushRange: true
       }
     };
- $scope.netSlider = {
+    $scope.netSlider = {
       minValue: 0,
       maxValue: 100000,
       options: {
@@ -329,7 +399,7 @@ angular.module('collegeScorecardApp')
       }
     };
     $scope.tuitionSlider = {
-    minValue: 0,
+      minValue: 0,
       maxValue: 100000,
       options: {
 
@@ -344,51 +414,52 @@ angular.module('collegeScorecardApp')
 
     $scope.priceSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
 
     $scope.debtSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
 
     $scope.loanSlider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 1,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
-        maxRange: 100,
+        ceil: 1,
+        step: 0.1,
+        minRange: 0.1,
+        maxRange: 1,
         pushRange: true
       }
     };
 
     $scope.earning7Slider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 100000,
       options: {
+
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
+        ceil: 100000,
+        step: 1000,
+        minRange: 1000,
         maxRange: 100,
         pushRange: true
       }
@@ -396,42 +467,27 @@ angular.module('collegeScorecardApp')
 
     $scope.earning3Slider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 100000,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
+        ceil: 100000,
+        step: 1000,
+        minRange: 1000,
         maxRange: 100,
         pushRange: true
       }
     };
-
     $scope.earning1Slider = {
       minValue: 0,
-      maxValue: 100,
+      maxValue: 100000,
       options: {
         floor: 0,
-        ceil: 100,
-        step: 1,
-        minRange: 10,
+        ceil: 100000,
+        step: 1000,
+        minRange: 1000,
         maxRange: 100,
         pushRange: true
       }
     };
 
-// .directive('bsTooltip', function(){
-//     return {
-//         restrict: 'A',
-//         link: function(scope, element, attrs){
-//             $(element).hover(function(){
-//                 // on mouseenter
-//                 $(element).tooltip('show');
-//             }, function(){
-//                 // on mouseleave
-//                 $(element).tooltip('hide');
-//             });
-//         }
-//     };
-// });
   });
--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -13,7 +13,9 @@ angular.module('collegeScorecardApp')
       // 
 
       // Public API here
-      return $resource('https://api.data.gov/ed/collegescorecard/v1/schools.json?2014.student.size__range=:minClass..:maxClass&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ', {}, {
+      return $resource('https://api.data.gov/ed/collegescorecard/v1/schools.json?2014.student.size__range=minClass..maxClass&2014.student.demographics.women__range=minWomen..maxWomen&2014.student.demographics.race_ethnicity.white__range=minWhite..maxWhite&2014.student.students_with_pell_grant__range=minPell..maxPell&2014.student.share_firstgeneration__range=minFirst..maxFirst&2014.student.retention_rate.overall.full_time__range=minFull..maxFull&2014.admissions.admission_rate.overall__range=minAdmit..maxAdmit&2014.admissions.sat_scores.average.overall__range=minSAT..maxSAT&2014.aid.median_debt_suppressed.completers.overall__range=minDebt..maxDebt&2014.aid.students_with_any_loan__range=minLoan..maxLoan&2014.cost.avg_net_price.overall__range=minPrice..maxPrice&2014.cost.tuition.in_state__range=minTuition..maxTuition&2014.cost.attendance.academic_year__range=minCost..maxCost&2007.completion.completion_rate_4yr_150nt__range=minComp..maxComp&2012.earnings10_yrs_after_entry.median__range=minEarn..maxEarn&2012.earnings10_yrs_after_entry.percent_greater_than_25000__range=minHS..maxHS&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ&_per_page=100', {}, {
+       // https://api.data.gov/ed/collegescorecard/v1/schools.json?2014.student.size__range=:minClass..:maxClass&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ
+ 
         //https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=:query&api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ
         //https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ&2014.student.size__range=:minSize..:maxSize
         //https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=xqfqZmKUIPqNYDG8WVRvF3079XzgGgXtyJSloKzQ&2014.student.size__range=0..60000&2014.student.demographics.women__range=0..1&2014.student.demographics.race_ethnicity.white__range=0..1&2014.student.students_with_pell_grant__range=0..1&2014.student.share_firstgeneration__range=0..1&2014.student.retention_rate.overall.full_time__range=0..1&2014.admissions.admission_rate.overall__range=0..1&2014.admissions.sat_scores.average.overall__range=0..1600&2014.aid.median_debt_suppressed.completers.overall__range=0..1000000&2014.aid.students_with_any_loan__range=0..1&2014.cost.avg_net_price.overall__range=0..100000&2014.cost.tuition.in_state__range=0..100000&2014.cost.attendance.academic_year__range=0..100000&2007.completion.completion_rate_4yr_150nt__range=0..1&_per_page=50
@@ -24,7 +26,37 @@ angular.module('collegeScorecardApp')
           params: {
 //             query: 'Seattle',
             minClass: 0,
-            maxClass: 60000
+            maxClass: 60000,
+            minWomen:0,
+            maxWomen:1,
+            minWhite:0,
+            maxWhite:1,
+            minPell:0,
+            maxPell:1,
+            minFirst:0,
+            maxFirst:1,
+            minFull:0,
+            maxFull:1,
+            minAdmit:0,
+            maxAdmit:1,
+            minSAT:0,
+            maxSAT:1600,
+            minDebt:0,
+            maxDebt:1000000,
+            minLoan:0,
+            maxLoan:1,
+            minPrice:0,
+            maxPrice:100000,
+            minTuition:0,
+            maxTuition:100000,
+            minCost:0,
+            maxCost:100000,
+            minComp:0,
+            maxComp:1,
+            minEarn:0,
+            maxEarn:100000,
+            minHS:0,
+            maxHS:1,
           },
            isArray: false
         }
